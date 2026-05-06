331f289faa15f9f7acc2d0b7ba96eaf1487bc592
cadoman1313-tech <cadoman1313@gmail.com>
2026-05-06 21:11:18 +0200

    Fix GitHub Pages refresh routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './app/App.jsx';
 import { MarketplaceProvider } from './state/MarketplaceContext.jsx';
 import './styles/index.css';
@@ -8,9 +8,9 @@ import './styles/index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MarketplaceProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </MarketplaceProvider>
   </React.StrictMode>,
 );
